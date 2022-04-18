dbac36c6396952eb99cd771c163f528757459d57
Mohammed Diaa <mohammeddiaa2000@gmail.com>
2022-04-18 17:27:02 +0200

    Add stderr redirection to path-executables test

--- tests/path-execuables.sh
+++ tests/path-execuables.sh
@@ -1,3 +1,3 @@
 echo "- path-executables:"
-for d in `echo $PATH | tr ":" "\n"`; do find $d -name "*.sh" | sed 's/^/ - /g' 2>/dev/null; done
-for d in `echo $PATH | tr ":" "\n"`; do find $d -type f -executable | sed 's/^/ - /g' 2>/dev/null; done
+for d in `echo $PATH | tr ":" "\n"`; do find $d -name "*.sh" 2>/dev/null | sed 's/^/ - /g'; done
+for d in `echo $PATH | tr ":" "\n"`; do find $d -type f -executable 2>/dev/null | sed 's/^/ - /g'; done
