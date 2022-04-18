74a872975a39942e183f8821ea4882ed17e5a740
Mohammed Diaa <mohammeddiaa2000@gmail.com>
2022-04-18 21:06:14 +0200

    Install lsof within the listening ports test in case it's missing

--- tests/listening-ports.sh
+++ tests/listening-ports.sh
@@ -1,2 +1,6 @@
+exec 3>&2
+exec 2> /dev/null
+(apt-get update > /dev/null && apt-get install lsof -y > /dev/null) || apk add --quiet lsof > /dev/null
+
 echo "- listening-ports:"
 lsof -Pni | egrep "(UDP|LISTEN)" | awk '{print $1": "$2}' | sort -u | sed 's/^/  - /g'
