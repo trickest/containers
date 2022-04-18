4f92f24ffee0d4d2ad6f85dd4496d1e82e8c4669
Mohammed Diaa <mohammeddiaa2000@gmail.com>
2022-04-18 17:50:03 +0200

    Redirect apk add output

--- tests/ssl-certs.sh
+++ tests/ssl-certs.sh
@@ -1,6 +1,6 @@
 exec 3>&2
 exec 2> /dev/null
-(apt-get update > /dev/null && apt-get install openssl -y > /dev/null) || apk add --quiet openssl
+(apt-get update > /dev/null && apt-get install openssl -y > /dev/null) || apk add --quiet openssl > /dev/null
 
 echo "- ssl-certs:"
 cat /etc/ssl/certs/ca-certificates.crt | awk -v cmd='openssl x509 -noout -subject' '
